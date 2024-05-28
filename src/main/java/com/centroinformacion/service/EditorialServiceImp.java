783c2e9a2affcc8ac3a64c81cbe58b2cd32e5af0
Kira Sayuri <kirasayuri74@gmail.com>
2024-05-27 23:46:18 -0500

    Ejercicio Resuelto

---
+++ src/main/java/com/centroinformacion/service/EditorialServiceImp.java
@@ -0,0 +1,30 @@
+package com.centroinformacion.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.centroinformacion.entity.Editorial;
+import com.centroinformacion.repository.EditorialRepository;
+
+
+@Service
+public class EditorialServiceImp implements EditorialService {
+
+	@Autowired	
+	private EditorialRepository repository;
+	
+	@Override
+	public List<Editorial> listaEditorial() {
+		return repository.findAll();
+	}
+
+	@Override
+	public Editorial insertaActualizaEditorial(Editorial obj) {
+		
+		return repository.save(obj);
+	}
+
+
+}
